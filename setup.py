1c84c57700a43c4353563d1b214d51da23aa297e
Nikhil Kumar Bharti <nikhilkumarbharti.2001@gmail.com>
2025-03-13 18:01:32 +0530

    Project structure and venv ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Nikhil Kumar Bharti",
+    author_email="nikhilkumarbharti.2001@gmail.com",
+    packages=find_packages()
+)
